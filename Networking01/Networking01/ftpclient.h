8c5249381f351447b6f8a7cffcc3e2bb34871c88
Yanki Muslu <yamuslu@gmail.com>
2017-03-03 11:56:15 +0100

    Skeleton

--- Networking01/Networking01/ftpclient.h
+++ Networking01/Networking01/ftpclient.h
@@ -1,11 +1,20 @@
 #pragma once
 #include <string.h>
+#include <winsock.h>
 typedef char* string;
 
-void connect(string host, int port);
-void disconnect();
+class Client {
+public:
+	void connect(string host, int port);
+	void disconnect();
 
-void download(string target);
-void displayFile(string filePath);	//Reads and prints up to 1KB of file
+	void download(string target);
+	void displayFile(string filePath);	//Reads and prints up to 1KB of file
 
-void upload(string filePath, string targetDir);
+	void upload(string filePath, string targetDir);
+private:
+	int socket;
+	sockaddr_in saddr;
+	WSADATA wlib;
+	int success;
+};
